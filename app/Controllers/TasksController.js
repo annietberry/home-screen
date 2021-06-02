4ffae8ec1d349f04e42ce2625952fa6f01f03176
annietberry <tombariannie@gmail.com>
2021-06-02 09:23:08 -0700

    Fixed Button Colors

--- app/Controllers/TasksController.js
+++ app/Controllers/TasksController.js
@@ -3,8 +3,7 @@ import { tasksService } from "../Services/TasksService.js"
 import { ProxyState } from '../AppState.js'
 
 import { loadState } from '../Utils/LocalStorage.js'
-// import TasksController from "./TasksController.js"
-import ListsController from "./ListsController.js"
+
 
 function _draw() {
   console.log('drawing your lists')
@@ -21,7 +20,7 @@ function _draw() {
             <div class="col d-flex ">${task.description}
             </div>
         </label>
-        <button onclick="app.listsController.clickSweetTask('${task.description}')" class="btn btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></button>
+        <button onclick="app.listsController.clickSweetTask('${task.description}')" class="btn btn-success"><i class="fa fa-trash" aria-hidden="true"></i></button>
         <br>
     </li>`
   })
@@ -32,13 +31,12 @@ function _draw() {
           <label for="name"></label>
           <input required minlength="3" maxlength="50" type="text" name="name" id="" class="form-control"
               placeholder="" aria-describedby="helpId">
-          <button class="btn btn-primary btn-block text-dark mt-1"
+          <button class="btn btn-success btn-block text-dark mt-1"
               "> Add task</button>
       </form>
   </div>
   </div>
   `
-
   taskElm.innerHTML = template
 }
 
@@ -62,7 +60,6 @@ export default class TasksController {
     let form = event.target
     let newTask = {
       description: form.name.value,
-
     }
     tasksService.addTask(newTask)
   }
@@ -75,23 +72,5 @@ export default class TasksController {
   updateTask(taskId) {
     console.log("task checkbox")
     tasksService.updateTask(taskId)
-  }
-
-
-  // checkBoxes() {
-  //   var inputElems = document.getElementById("input")
-
-  //   for (var i = 0; i < inputElems.length; i++) {
-  //     if (inputElems[i].type == "checkbox" && inputElems[i].checked == true) {
-  //       count
-  //       count++;
-  //     }
-  //   }
-  // }
-
-  checkBoxes() {
-    if (document.getElementById('input').checked == true) {
-      var checkCount = checkCount++
-    }
   }
 }
--- app/Services/TasksService.js
+++ app/Services/TasksService.js
@@ -6,29 +6,6 @@ import { sandboxApi } from "./AxiosService.js";
 
 class TasksService {
 
-  // async getCaughtPokemon(){
-  //   try {
-  //     let res = await sandboxApi.get()
-  //     console.log('my pokemon', res);
-  //     ProxyState.caughtPokemon = res.data.map(cp => new Pokemon(cp))
-  //     console.log("my caught pokemon", ProxyState.caughtPokemon);
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-
-  // }
-
-  // async catchPokemon() {
-  //   try {
-  //     let res = await sandboxApi.post("", ProxyState.activePokemon)
-  //     console.log("catch pokemon", res)
-  //     this.getCaughtPokemon()
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-
   async addTask(newTask) {
     try {
       let res = await sandboxApi.post('', new Task(newTask))
@@ -41,28 +18,6 @@ class TasksService {
 
   }
 
-  // async releasePokemon(pokemonName){
-  //   try {
-  //     let foundPokemon = ProxyState.caughtPokemon.find(cp => cp.name == pokemonName)
-  //     console.log(foundPokemon)
-  //     let res = await sandboxApi.delete("/" + foundPokemon.id)
-  //     // ProxyState.caughtPokemon = ProxyState.caughtPokemon.filter(cp => cp.id != ProxyState.activePokemon.id)
-  //     this.getCaughtPokemon()
-  //     console.log(res)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-
-  // try {
-  //   let res = await sandboxApi.get()
-  //   console.log('my pokemon', res);
-  //   ProxyState.caughtPokemon = res.data.map(cp => new Pokemon(cp))
-  //   console.log("my caught pokemon", ProxyState.caughtPokemon);
-  // } catch (error) {
-  //   console.log(error)
-  // }
   async getTasks() {
     try {
       let res = await sandboxApi.get()
@@ -97,18 +52,6 @@ class TasksService {
     console.log("task removed")
   }
 
-  //ADD IN FOR COMPLETED.
-  // .post('object ', updatew/updatedinfo)
-
-  // async updateTask(task) {
-  //     // @ts-ignore
-  //     let res = await axios.put(sandboxApi + task.co, formData)
-
-  //     let indexOfCarToBeUpdated = ProxyState.cars.findIndex(c => c.id == formData.id)
-  //     ProxyState.cars.splice(indexOfCarToBeUpdated,1, new Car(res.data))
-  //     ProxyState.cars = ProxyState.cars
-  // }
-
   async updateTask(taskId) {
     try {
       let foundTask = await ProxyState.tasks.find(t => t.id == taskId)
@@ -122,12 +65,7 @@ class TasksService {
     catch (error) {
       console.log(error)
     }
-    // get info to change
-    //access id of thing needed to be changed
-    //axios put payload(id, completed)
-    //check response
-    //update proxyState with what you recieved from the response.
-    // let res = await axios.put()
+
     console.log("updating")
   }
 }
